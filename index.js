d8dc5a2c5429b8e90009fadceb438adaa2a67c25
Loki <jjdevelpersmtz@gmail.com>
2022-04-21 17:28:15 -0500

    30

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function myAsyncFunction(req,res){
 
 const cluster = await Cluster.launch({
                 concurrency: Cluster.CONCURRENCY_CONTEXT,
-                maxConcurrency: 50,
+                maxConcurrency: 30,
                 puppeteerOptions: {
                     headless: true,
                  args: ['--no-sandbox']
